b2d9be0f65995a52f7f6fd5b3e5262e6bfbb7c35
DarioFalzone <darioefalzone95@gmail.com>
2021-04-19 23:15:12 -0300

    P07 - npm script Nodemon created successfully

--- back-js/index.js
+++ back-js/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
-
 const app = express();
 
 app.listen(4000, () => {
-	console.log('Aplicación corriendo en el puerto 4000');
+	console.log('Aplicación corriendo exitosamente en el puerto 4000');
 })
+
+//Routes
+// app.get('/login', (request, response) => {
+// 	console.log(__filename);
+// 	response.sendFile(path.resolve(__filename, 'C:/Users/Dario%20Falzone/Desktop/RadiumRocket/problem-10/public/login.html'))
+// })
+
+// app.get('/register', (request, response) => {
+// 	console.log(__filename);
+// 	response.sendFile(path.resolve(__filename, '/c/Users/Dario Falzone/Desktop/RadiumRocket/problem-10/public/register.html'))
+// })
