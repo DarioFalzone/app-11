93457876e376d6f9f68c5aebabbb35bab181f585
DarioFalzone <darioefalzone95@gmail.com>
2021-04-20 02:30:21 -0300

    P10 / P11 - Register.js Finish

--- back-js/index.js
+++ back-js/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
 app.listen(4000, () => {
 	console.log('Aplicación corriendo exitosamente en el puerto 4000');
 })
 
-
-// const handleRegister = () => {
-
-// }
+const handleRegister = () => {
+	//Routes
+	app.post('/register', (request, response) => {
+	response.json({	result: 'Ey! You have successfully registered !! :D'})
+	})
+}
 
 // const handleLogin = () => {
 
@@ -16,14 +24,12 @@ app.listen(4000, () => {
 // }
 
 //Routes
-app.get('/register', (request, response) => {
-	response.json({
-		datos: 'Estas en la ruta correspondiente al: REGISTRO'
-	})
+app.post('/register', (request, response) => {
+	response.json({	result: 'Ey! You have successfully registered !! :D'})
 })
 
-app.get('/login', (request, response) => {
-	response.json({
-		datos: 'Estas en la ruta correspondiente al: LOGIN'
-	})
-})
+// app.get('/login', (request, response) => {
+// 	response.json({
+// 		datos: 'Estas en la ruta correspondiente al: LOGIN'
+// 	})
+// })
